2173769531433a6d20188fb9b4b475a34056d3b5
ovalkin <egorvasilevskiy585@gmail.com>
2024-12-14 05:19:35 +0500

    feat: добавил эндпоинты

---
+++ DvachApi.Common/GetApiEndpoints.cs
@@ -0,0 +1,26 @@
+﻿namespace DvachApi.Common;
+
+public static class GetApiEndpoints
+{
+    private static string _url = "";
+    private static readonly HttpClient Client = new();
+    public static Task<HttpResponseMessage> GetBoards()
+    {
+        _url = "https://2ch.hk/api/mobile/v2/boards";
+        var response =  Client.GetAsync(_url);
+        return response;
+    }
+    public static Task<HttpResponseMessage> GetThreads(string boardId)
+    {
+        _url = $"https://2ch.hk/{boardId}/catalog.json";
+        var response =  Client.GetAsync(_url);
+        return response;
+    }
+
+    public static Task<HttpResponseMessage> GetPosts(string boardId, int threadNum)
+    {
+        _url = $"https://2ch.hk/{boardId}/res/{threadNum}.json";
+        var response =  Client.GetAsync(_url);
+        return response;
+    }
+}
